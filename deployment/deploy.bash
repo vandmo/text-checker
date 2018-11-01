d91c3e1ec95e0c4df2dee088445465c1c6f7031a
Mikael Vandmo <mikael.vandmo@wirelesscar.com>
2018-11-01 10:11:44 +0100

    Create deploy script

---
+++ deployment/deploy.bash
@@ -0,0 +1,20 @@
+set -euxo pipefail
+
+mvn versions:set \
+  --file maven-plugin/pom.xml \
+  --batch-mode \
+  --define newVersion=${TRAVIS_TAG}
+
+gpg2 --keyring=${TRAVIS_BUILD_DIR}/pubring.gpg --no-default-keyring --import deployment/signingkey.asc
+gpg2 --allow-secret-key-import --keyring=${TRAVIS_BUILD_DIR}/secring.gpg --no-default-keyring --import deployment/signingkey.asc
+
+mvn clean deploy \
+  --file maven-plugin/pom.xml \
+  --batch-mode \
+  --settings deployment/settings.xml \
+  --define gpg.executable=gpg2 \
+  --define gpg.keyname=B07746099C297F239C1C4343E16B0657F1774582 \
+  --define gpg.passphrase=${PASSPHRASE} \
+  --define gpg.publicKeyring=${TRAVIS_BUILD_DIR}/pubring.gpg \
+  --define gpg.secretKeyring=${TRAVIS_BUILD_DIR}/secring.gpg \
+  --define maven.skip.install=true
